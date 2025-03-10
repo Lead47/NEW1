bd5a3ffffce3d20545425e3adb5c1350c2e3836f
Anjum143 <hussainnawazkb5500@gmail.com>
2025-03-10 10:33:46 +0500

    UPDATED

--- RTNS/settings.py
+++ RTNS/settings.py
@@ -161,8 +161,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'bsf2003459@ue.edu.pk'
-EMAIL_HOST_PASSWORD = 'vhlaBtha@2'
+EMAIL_HOST_USER = 'hussainnawazkb5500@gmail.com'
+EMAIL_HOST_PASSWORD = 'HA678954'
 
 ADMIN_EMAIL='adeebhassi@gmail.com'
 # settings.py
